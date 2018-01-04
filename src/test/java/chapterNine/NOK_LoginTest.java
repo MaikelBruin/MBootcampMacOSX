74737f5aecdf58eeb75cec7eefc250ade81902fa
MaikelBruin <maikelbruin@gmail.com>
2018-01-04 16:08:59 +0100

    Bootcamp dag 4 NOK testen

--- src/test/java/chapterNine/NOK_LoginTest.java
+++ src/test/java/chapterNine/NOK_LoginTest.java
@@ -4,34 +4,119 @@ import chapterSix.TestShopScenario;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.LoginPage;
 
 public class NOK_LoginTest extends TestShopScenario {
     String email = "maikel.bruin@polteq.com";
-    String pwd = "lalalal";
-    String expectedLoginMessage =
-            "Welcome to your account. Here you can manage all of your personal information and orders.";
+    String pwd = "bootcamp";
+    String pwdWrong = "lalalal";
 
     @Test
-    public void performNOKLoginTest() {
+    public void wrongAuthenticationTest() {
         LoginPage loginPage = new LoginPage(driver);
+        String expectedErrorMessage = "Authentication failed.";
 
-//        loginPage.doLogin(email, pwd);
 
-
-        if (loginPage.isLoggedOut() == true) {
-            loginPage.doLogin(email, pwd);
-        } else {
-            System.out.println("(Other) user was already logged in, relogging");
+        if (loginPage.isLoggedOut() == false) {
             loginPage.doLogout();
-            loginPage.doLogin(email, pwd);
         }
 
-        String actualLoginMessage = driver.findElement(By.className("info-account")).getText();
-
+        String actualErrorMessage = loginPage.doNOKLogin(email, pwdWrong);
 
         //Assertions
-        Assertions.assertThat(actualLoginMessage)
-                .as("Login message is niet zoals verwacht!").contains(expectedLoginMessage);
+        Assertions.assertThat(actualErrorMessage)
+                .as("Error message is niet zoals verwacht!").contains(expectedErrorMessage);
+    }
+
+    @Test
+    public void wrongEmailFormatTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.toLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        if (loginPage.isLoggedOut() == false) {
+            loginPage.doLogout();
+        }
+
+        String emailWrongFormat = "lalala";
+
+        Boolean formErrorMessage = loginPage.doNOKEmail(emailWrongFormat);
+
+        //Assertions
+        Assertions.assertThat(formErrorMessage)
+                .as("Error message niet getoond!").isTrue();
+    }
+
+    @Test
+    public void emptyPasswordTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.toLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        if (loginPage.isLoggedOut() == false) {
+            loginPage.doLogout();
+        }
+
+        String expectedErrorMessage = "Password is required.";
+        String actualErrorMessage = loginPage.doNOKLogin(email, "");
+
+        //Assertions
+        Assertions.assertThat(actualErrorMessage)
+                .as("Error message is not as expected!").contains(expectedErrorMessage);
+    }
+
+    @Test
+    public void emptyEmailTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.toLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        if (loginPage.isLoggedOut() == false) {
+            loginPage.doLogout();
+        }
+
+        String expectedErrorMessage = "An email address required.";
+        String actualErrorMessage = loginPage.doNOKLogin("", "");
+
+        //Assertions
+        Assertions.assertThat(actualErrorMessage)
+                .as("Error message is not as expected!").contains(expectedErrorMessage);
+    }
+
+    @Test
+    public void tooShortPwdTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.toLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        if (loginPage.isLoggedOut() == false) {
+            loginPage.doLogout();
+        }
+
+        String shortPwd = "1234";
+        Boolean formErrorMessage = loginPage.doNOKPwd(email,shortPwd);
+
+        //Assertions
+        Assertions.assertThat(formErrorMessage)
+                .as("Error message niet getoond!").isTrue();
+    }
+
+    @Test
+    public void nonExistentUserTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.toLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+
+        if (loginPage.isLoggedOut() == false) {
+            loginPage.doLogout();
+        }
+
+
     }
 }
