d1b6353b5e3c99019cdd10d72926b993665d4c9b
MaikelBruin <maikelbruin@gmail.com>
2018-01-05 13:13:44 +0100

    Bootcamp dag 5, API testjes

--- src/test/java/api/APIDemo.java
+++ src/test/java/api/APIDemo.java
@@ -1,11 +1,19 @@
 package api;
 
 
+import io.restassured.response.ResponseBody;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class APIDemo {
+
+    int season = 2017;
+    int raceNumber = 2;
 
     @Test
     public void currentF1Results() {
@@ -15,11 +23,28 @@ public class APIDemo {
     @Test
     public void verstappenResults() {
         given().when().get("http://ergast.com/api/f1/2016/drivers/verstappen/results.json").then().statusCode(200);
+        given().when().get("http://ergast.com/api/f1/2016/drivers/verstappen/results.json").getBody().prettyPrint();
+
+
+        given().when().get("http://ergast.com/api/f1/2016/drivers/verstappen/results.json").then()
+                .body("MRDATA.RaceTable.driverId", equalTo("verstappen"));
+
     }
 
     @Test
-    public void andereResults() {
-//        given().when().
+    public void pathParam() {
+        given().pathParam("season", 2017)
+                .when().get("http://ergast.com/api/f1/{season}/last/results.json")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void multiplePathParam() {
+        given()
+                .pathParam("season", season)
+                .pathParam("raceNumber", raceNumber)
+                .when().get("http://ergast.com/api/f1/{season}/{raceNumber}/results.json")
+                .then().statusCode(200);
     }
 
 }
